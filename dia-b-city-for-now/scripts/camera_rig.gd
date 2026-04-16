caba870093a4d79fe35750892b2b27edeeb7468e
darriusisart <110921782+darriusisart@users.noreply.github.com>
2026-04-16 13:05:13 -0500

    interior of school

--- dia-b-city-for-now/scripts/camera_rig.gd
+++ dia-b-city-for-now/scripts/camera_rig.gd
@@ -4,13 +4,18 @@ extends Node3D
 @onready var camera_3d = $Camera3D
 
 @export var stage_dimentions:Vector2
+@onready var debug_target: Node3D = get_node_or_null("MeshInstance3D2")
 
 func _process(delta):
+	if player == null or camera_3d == null:
+		return
+
 	position = lerp(position,player.position,delta*10.0)
 	position.x = clampf(position.x,-stage_dimentions.x/2,stage_dimentions.x/2)
 	position.z = clampf(position.z,-stage_dimentions.y/2,stage_dimentions.y/2)
 	
 	camera_3d.look_at(((player.position+position)/2)+Vector3.UP,Vector3.UP)
 	
-	$MeshInstance3D2.global_position = ((player.position+position)/2)+Vector3.UP
+	if debug_target != null:
+		debug_target.global_position = ((player.position+position)/2)+Vector3.UP
 	

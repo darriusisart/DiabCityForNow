42f3bfb89159dc445f3cec296640652d6a400717
mblumpkin <mblumpkin2@gmail.com>
2026-06-11 22:03:30 -0500

    Adding jumping for all views, deleted falling
    I added spritesheets for the front and back view jumping. I made the falling animations for all views but haven't added it yet to the script. All of the script so far now runs on what the last_view is for each of the animations. I'll have to add another variable for what character the player selects in the future.

--- dia-b-city-for-now/Scenes/levels/madison_player.gd
+++ dia-b-city-for-now/Scenes/levels/madison_player.gd
@@ -49,21 +49,7 @@ func update_animation(input_dir: Vector2):
 	# yet for the other views
 	if not is_on_floor():
 		if velocity.y > 0:
-			match last_view:
-				"front":
-					play_anim("jump_front")
-				"back":
-					play_anim("jump_side") # temporary until I add a back-view jump
-				"side":
-					play_anim("jump_side")
-		else:
-			match last_view:
-				"front":
-					play_anim("fall_front")
-				"back":
-					play_anim("fall_side") # also temp
-				"side":
-					play_anim("fall_side")
+			play_anim("jump_" + last_view)
 
 		sprite.speed_scale = 1.0
 		return

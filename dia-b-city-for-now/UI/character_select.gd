4b1546fb98061e6bec3a472c85e9f1a95077fca1
mblumpkin <mblumpkin2@gmail.com>
2026-07-14 10:21:18 -0500

    Added Beau character with animations
    Added Beau as a playable character will all animations and views

--- dia-b-city-for-now/UI/character_select.gd
+++ dia-b-city-for-now/UI/character_select.gd
@@ -4,12 +4,14 @@ var selected_character := "west"
 
 @onready var west_button = $CanvasLayer/Control/VBoxContainer/HBoxContainer/WestButton
 @onready var suki_button = $CanvasLayer/Control/VBoxContainer/HBoxContainer/SukiButton
+@onready var beau_button = $CanvasLayer/Control/VBoxContainer/HBoxContainer/BeauButton
 @onready var start_button = $CanvasLayer/Control/VBoxContainer/StartButton
 @onready var selected_label = $CanvasLayer/Control/VBoxContainer/SelectedLabel
 
 func _ready():
 	west_button.pressed.connect(_on_west_pressed)
 	suki_button.pressed.connect(_on_suki_pressed)
+	beau_button.pressed.connect(_on_beau_pressed)
 	start_button.pressed.connect(_on_start_pressed)
 
 	selected_label.text = "Selected: West"
@@ -23,6 +25,11 @@ func _on_suki_pressed():
 	selected_character = "suki"
 	selected_label.text = "Selected: Suki"
 	print("Selected Suki")
+	
+func _on_beau_pressed():
+	selected_character = "beau"
+	selected_label.text = "Selected: Beau"
+	print("Selected Beau")
 
 func _on_start_pressed():
 	Global.selected_character = selected_character
